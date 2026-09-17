e6d2106df2e15bc60532ac0d3c82ee6d705c18e0
vrajgelani255-max <vrajgelani255@gmail.com>
2026-09-17 23:04:55 +0530

    Add files via upload

---
+++ test_vehicle.py
@@ -0,0 +1,12 @@
+from vehicle import vehicle_command
+
+print("Sixth Sense Vehicle Test")
+print("------------------------")
+
+vehicle_command("FORWARD")
+vehicle_command("LEFT")
+vehicle_command("RIGHT")
+vehicle_command("STOP")
+
+print("------------------------")
+print("Test completed successfully")
